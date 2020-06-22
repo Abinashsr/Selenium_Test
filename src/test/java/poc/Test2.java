6f582498b7188317cabab1c06b2bdc478adf124c
Abinash Smruti Ranjan <abinash.smruti.ranjan@ericsson.com>
2020-06-22 19:28:32 +0530

    second commit

--- src/test/java/poc/Test2.java
+++ src/test/java/poc/Test2.java
@@ -8,10 +8,10 @@ import org.testng.annotations.Test;
 public class Test2 {
 	@Test
 	public void mymain() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\eabismr\\Desktop\\New folder\\ch83\\chromedriver_win32 (1)\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\eabismr\\Desktop\\New folder\\15Jun Chrome driver - 83.0.4103.97\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver(); 
 		//driver.get("https://www.guru99.com");
-		driver.get("http://54.165.42.55:8082/webapp/welcome.jsp");
+		driver.get("http://35.169.85.39:8082/webapp/welcome.jsp");
 		driver.findElement(By.name("username")).sendKeys("AB1");
 		driver.findElement(By.name("password")).sendKeys("12345");
 		driver.findElement(By.xpath("/html/body/form/input[3]")).click();
